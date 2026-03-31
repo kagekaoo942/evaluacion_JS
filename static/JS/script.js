244dd3dbbf98805b119242f6520872d1a7d287c7
kagekaoo942 <martinacevedo@liceovvh.cl>
2026-03-31 11:39:41 -0300

    terminar

---
+++ static/JS/script.js
@@ -0,0 +1,9 @@
+/*1. Módulo de Tesorería: Pago de Permisos (Cálculos y Retorno)
+El vecino ingresa el monto de su permiso de circulación para saber cuánto pagará tras el descuento municipal.
+Lógica: Si el monto es mayor a $70.000, el descuento es del 15%. Si es menor, es del 5%.
+Regla: La Función Principal debe mostrar el total a pagar con un signo de peso ($).
+*/
+
+function pagoPermisos(){
+    
+}
